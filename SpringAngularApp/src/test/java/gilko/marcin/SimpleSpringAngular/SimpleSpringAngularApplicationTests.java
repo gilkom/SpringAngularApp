ce90850aa412fb5c5427a3d28beafbc74b38ffdd
gilkom <gilek06@op.pl>
2020-11-02 11:56:57 +0100

    create project

---
+++ SpringAngularApp/src/test/java/gilko/marcin/SimpleSpringAngular/SimpleSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package gilko.marcin.SimpleSpringAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
